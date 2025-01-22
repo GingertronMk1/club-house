a9c32a9ed7845f25839e74561706cd4fd1bdf507
Jack Ellis <jackellis1504@gmail.com>
2025-01-22 21:32:01 +0000

    Adding clubs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +26,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        /**
+         * If we're not in production make models all strict and log queries
+         */
+        if (!\App::isProduction()) {
+            Model::shouldBeStrict();
+            DB::listen(function (QueryExecuted $query) {
+                Log::info(
+                    'Query Executed', [
+                        'sql' => $query->sql,
+                        'bindings' => $query->bindings,
+                        'time' => $query->time
+                ]);
+            });
+        }
     }
 }
---
+++ resources/js/Components/Forms/Club/Form.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Club {
+    name: string;
+    description: string;
+    address: string;
+    main_colour: string;
+    secondary_colour: string;
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
