430942650b87ad292d5c4fb3c2d798a08150480c
Jack Ellis <jackellis1504@gmail.com>
2025-01-26 15:12:02 +0000

    Add and amend tests
     M app/Policies/ClubPolicy.php
     M stubs/policy.stub
    AM tests/Unit/Club/ClubPolicyTest.php
    AM tests/Unit/Club/ClubRequestTest.php

--- app/Policies/ClubPolicy.php
+++ app/Policies/ClubPolicy.php
@@ -12,7 +12,7 @@ class ClubPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -20,7 +20,7 @@ class ClubPolicy
      */
     public function view(User $user, Club $club): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -28,7 +28,7 @@ class ClubPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -36,7 +36,7 @@ class ClubPolicy
      */
     public function update(User $user, Club $club): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -44,7 +44,7 @@ class ClubPolicy
      */
     public function delete(User $user, Club $club): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -52,7 +52,7 @@ class ClubPolicy
      */
     public function restore(User $user, Club $club): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -60,6 +60,6 @@ class ClubPolicy
      */
     public function forceDelete(User $user, Club $club): bool
     {
-        return false;
+        return true;
     }
 }
